8dd18e53b0b454da6c0b8976bb7bf23ba65e80f8
BaH96p0 <144206430+BaH96p0@users.noreply.github.com>
2023-10-16 11:20:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+# Задача №1
+# def list_reorder(ref_list): #
+#     names = ref_list[0]
+#     surnames = ref_list [1]
+#     pairs = []
+#     for i in range(len(names)):
+#         pair = []
+#         pair.append(names)
+#         pair.append(surnames)
+#         pairs.append(pair)
+#     return pairs #список списков в котором имена и фамилии идут парами 
+
+
+
+
+# Задача №2
+# def del_rep(nums) : #
+#     unique_nums = list(set(num))
+#     unique_nums.sort
+#     return unique_nums # исходный список с числами
+    
+
+
+
+# Задача №3
+# def lim_max(nums, limit):
+#     max_num = -1
+#     for num in nums:
+#         if  num < limit and num > max num:
+#             max_num = num
+#     return max_num # ограниченный максимум
+
+
+
+# Задача №4
+# def temp_cat(temp):
+#     if temp < -20 :
+#         return (0, '#', 'Холодно') #номер категории
+#     elif temp >= -20 and temp < 0:
+#         return (1, '#', 'Прохладно') #номер категории
+#     elif temp >= 0 and temp < 15:
+#         return (2, '#', 'Зябко')    #номер категории 
+#     elif temp >= 15 and temp < 25:
+#         return (3, '#', 'Тепло') #номер категории
+#     else:
+#         return (4, '#', 'Жарко') #номер категории
+
+
+
+
+# Задача №5
+# def check_phn(phones):
+#     result = []
+#     for phone in phones:
+#         phone = phone.replace(' ','').replace('-','').replace(')','')
+#     if len(phone) !=11:
+#         result.sppend(-1)
+#         continue
+#     for char in phone[1:]:
+#         if not char.isdigit:
+#             result.append(-1)
+#             break
+#     else: result.append(phone)
+#     return result # список номеров в целевом формате или -1
+    
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
